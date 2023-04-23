edb1aceca36dcd05d95a511eb40e941725c67c24
JUWONCALEB <63285430+juwoncaleb@users.noreply.github.com>
2023-04-23 22:50:26 +0100

    FIXES

--- next.config.js
+++ next.config.js
@@ -4,3 +4,26 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+// MINI-CSS-EXTRACT-PLUGIN
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Use mini-css-extract-plugin on the client-side only
+      config.plugins.push(new MiniCssExtractPlugin({
+        filename: 'static/css/[contenthash].css',
+        chunkFilename: 'static/css/[contenthash].css',
+        ignoreOrder: true,
+      }));
+      config.module.rules.push({
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      });
+    }
+    return config;
+  },
+};
